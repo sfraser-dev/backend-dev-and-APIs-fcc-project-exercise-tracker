ff3b7d51ebad3a9da0648f0d0e3cb9d4ea46c34a
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-15 18:37:47 +0000

    adding root level body-parsing middleware to get the data from the body of forms

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require("express");
 const app = express();
 
@@ -20,36 +22,56 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 });
 
 // ME START
-// middleware for getting body data (from forms)
+// middleware for getting body data (req.body.XXX) from forms
 const bodyParser = require("body-parser");
+// add it to the middleware stack before any app routes are defined, root level middleware
+let middlewareParse = bodyParser.urlencoded({ extended: false });
+app.use(middlewareParse);
 
-// mongoose setup (for accessing mongodb) 
+// mongoose setup (for accessing mongodb)
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGO_URI, {
+        dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("database connected.");
+    })
+    .catch((err) => console.error(err));
 // mongoose user schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true }, 
-    count: Number
+    username: { type: String, required: true },
+    count: Number,
 });
 // mongoose user model
 const User = mongoose.model("User", userSchema);
 // mongoose exercise schema
 const exerciseSchema = new mongoose.Schema({
-    userid: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, 
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
     description: String,
     duration: Number,
-    date: Date
+    date: Date,
 });
 // mongoose exercise model
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 // TODO: post a new user
+app.post("/api/users", (req, res) => {
+    const theUsername = req.body.username;
+    console.log(theUsername);
+    res.json({ hello: theUsername });
+});
 
 // TODO: get a list of users
 
 // TODO: post a new exercise
 
 // TODO: show the user logs
+
+// ME END
