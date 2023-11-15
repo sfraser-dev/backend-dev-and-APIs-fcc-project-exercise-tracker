ae1408d430b1d638f519a2a107ab80c0b4b8e8ef
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-15 22:59:01 +0000

    tidy up. passing first 3 FCC tests

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 "use strict";
 
+// require dotenv to handle environment variables (call asap hence put at top)
+require("dotenv").config();
+
+// require mongoose (ODM library for mongodb). call asap so near top
+const mongoose = require("mongoose");
+// require bluebird for mongoose promises
+mongoose.Promise = require("bluebird");
+
+// require express
 const express = require("express");
 const app = express();
 
-// add "cors" to the middleware stack (via app.use())
+// require "cors" and add to the middleware stack (via app.use())
 const cors = require("cors");
 app.use(cors());
 
-// handle environment variables
-require("dotenv").config();
-
-// middleware to access public folder and then load index page at root
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-});
-
-// app to listen on port 3000
-const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log("Your app is listening on port " + listener.address().port);
-});
-
-// ME START
 // middleware for getting body data (req.body.XXX) from forms
 const bodyParser = require("body-parser");
 // add it to the middleware stack before any app routes are defined, root level middleware
 let middlewareParse = bodyParser.urlencoded({ extended: false });
 app.use(middlewareParse);
 
-// mongoose setup (for accessing mongodb)
-const mongoose = require("mongoose");
+// middleware access to public folder for static css file
+app.use(express.static("public"));
+
+// load index.html at root route
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+// ME START
+// mongoose connect
 mongoose
     .connect(process.env.MONGO_URI, {
         dbName: process.env.DB_NAME,
@@ -61,11 +63,22 @@ const exerciseSchema = new mongoose.Schema({
 // mongoose exercise model
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
-// TODO: post a new user
+// post a new user
 app.post("/api/users", (req, res) => {
     const theUsername = req.body.username;
     console.log(theUsername);
-    res.json({ hello: theUsername });
+    const userObject = new User({ username: theUsername, count: 1 });
+    // mongoose model's save() function returns a promise (if no callback passed as a function to it)
+    const savePromise = userObject.save();
+    savePromise
+        .then(() => {
+            // FCC requested response of "_id" and "username"
+            res.json({ _id: userObject.id, username: theUsername });
+        })
+        .catch((saveErr) => {
+            console.error(saveErr);
+            res.json({ error: "save failed" });
+        });
 });
 
 // TODO: get a list of users
@@ -75,3 +88,10 @@ app.post("/api/users", (req, res) => {
 // TODO: show the user logs
 
 // ME END
+
+
+
+// app to listen on port 3000
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+});
